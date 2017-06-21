65bf576424e3e48bca3bc16d60a943c34f6a3343
Shivesh Navin <shiveshnavin@gmail.com>
2017-06-21 15:10:52 +0530

    minor fixes

--- app/src/main/java/com/hackathon/poluchecker/Login.java
+++ app/src/main/java/com/hackathon/poluchecker/Login.java
@@ -8,6 +8,7 @@ public class Login extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_login);
     }
 }
